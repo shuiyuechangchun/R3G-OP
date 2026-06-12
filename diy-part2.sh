f233f8ce9dc00de07cd811f81b2889fae7ff9238
shuiyuechangchun <48248824+shuiyuechangchun@users.noreply.github.com>
2026-06-12 10:50:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,5 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
